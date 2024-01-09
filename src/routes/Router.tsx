36b6139c6c3c7ee56fa19119f8dfcc338395aeca
Azam Gulov <gulovazam@gmail.com>
2024-01-10 00:00:13 +0500

    Add message board loader

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -16,6 +16,19 @@ export default function Router() {
         {
           path: "/message-board",
           element: <MessageBoard />,
+          loader: async () => {
+            const data = await fetch(
+              "https://top-members-only-api.fly.dev/message-board",
+              {
+                method: "GET",
+                credentials: "include",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              }
+            );
+            return data;
+          },
         },
       ],
     },
