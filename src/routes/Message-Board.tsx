062986694a774d907f4f6e69219d4ab125110861
Azam Gulov <gulovazam@gmail.com>
2024-01-15 23:36:05 +0500

    Fetch messages on mount
    add message component
    navigate user to login page if status error 401 or 403

--- src/routes/Message-Board.tsx
+++ src/routes/Message-Board.tsx
@@ -1,19 +1,59 @@
-import { useLoaderData } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function MessageBoard() {
-  const messages = useLoaderData();
+  const [messages, setMessages] = useState();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function fetchMessages() {
+      try {
+        const res = await fetch(
+          "https://top-members-only-api.fly.dev/message-board",
+          {
+            method: "GET",
+            mode: "cors",
+            credentials: "include",
+            headers: { "Content-Type": "application/json" },
+          }
+        );
+
+        if (res.ok) {
+          const data = await res.json();
+          setMessages(data);
+        }
+
+        if (res.status === 401 || res.status === 403) {
+          navigate("/");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchMessages();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div>
-      {messages.map((message) => {
-        return (
-          <div key={message.date + message.user._id}>
-            <div>{message.user.username}</div>
-            <div>{message.message}</div>
-            <div>{message.date}</div>
-          </div>
-        );
-      })}
+      {messages
+        ? messages.map((message) => {
+            return (
+              <Message key={message.date + message._id} message={message} />
+            );
+          })
+        : "No Messages"}
+    </div>
+  );
+}
+
+function Message({ message }) {
+  return (
+    <div className="user-message">
+      <div>{message.user.username}</div>
+      <div>{message.message}</div>
+      <div>{message.date}</div>
     </div>
   );
 }
