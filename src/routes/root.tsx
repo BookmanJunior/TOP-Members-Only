a5c817ef361c6d4cadc5272be21228a24f25beb6
Azam Gulov <gulovazam@gmail.com>
2024-01-15 23:34:59 +0500

    Remove login function
    change userId state

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,40 +1,11 @@
 import { useState } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import "../App.css";
 
 export default function Root() {
-  const [userId, setUserId] = useState("");
-  const [credentials, setCredentials] = useState({
-    username: "",
-    password: "",
-  });
+  const [user, setUser] = useState("");
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
-
-  async function login(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    setLoading(true);
-
-    try {
-      const res = await fetch("https://top-members-only-api.fly.dev/login", {
-        method: "POST",
-        mode: "cors",
-        credentials: "include",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(credentials),
-      });
-
-      if (res.ok) {
-        const { user } = await res.json();
-        setUserId(user);
-        navigate("/message-board");
-        setLoading(false);
-      }
-    } catch (error) {
-      setError(true);
-    }
-  }
 
   if (loading) {
     return <div>Loading...</div>;
@@ -44,11 +15,10 @@ export default function Root() {
     <>
       <Outlet
         context={{
-          userId,
-          setUserId,
-          credentials,
-          setCredentials,
-          login,
+          user,
+          setUser,
+          setLoading,
+          setError,
           error,
         }}
       />
