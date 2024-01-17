bf4672200fe9e344b51b01e2f6a72bd85039fc24
Azam Gulov <gulovazam@gmail.com>
2024-01-17 14:53:25 +0500

    Add sign up form
    add validation error component

---
+++ src/routes/Sign-Up.tsx
@@ -0,0 +1,105 @@
+import { useState } from "react";
+import { useNavigate, useOutletContext } from "react-router-dom";
+
+export default function SignUp() {
+  const [userCredentials, setUserCredentials] = useState({
+    username: "",
+    password: "",
+    "confirm-password": "",
+  });
+  const [errors, setErrors] = useState();
+  const { setUser } = useOutletContext();
+  const [submittedSignUp, setSubmittedSignUp] = useState(false);
+  const navigate = useNavigate();
+
+  function handleCredentialsChange(e, prop) {
+    setUserCredentials({ ...userCredentials, [prop]: e.target.value });
+  }
+
+  async function handleSignUp(e) {
+    e.preventDefault();
+    setSubmittedSignUp(true);
+
+    try {
+      const res = await fetch("http://localhost:3000/sign-up", {
+        method: "POST",
+        mode: "cors",
+        credentials: "include",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(userCredentials),
+      });
+
+      if (res.status >= 400) {
+        const err = await res.json();
+        setErrors(
+          err.reduce((errorObj, next) => {
+            errorObj[next.path] ??= [];
+            errorObj[next.path].push(next.msg);
+            return errorObj;
+          }, {})
+        );
+        return;
+      }
+
+      const { user } = await res.json();
+      setUser(user);
+      navigate("/message-board");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmittedSignUp(false);
+    }
+  }
+
+  return (
+    <form
+      onSubmit={handleSignUp}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "0.5em",
+        textAlign: "left",
+        width: "350px",
+      }}
+    >
+      <label htmlFor="username">Username:</label>
+      <input
+        type="text"
+        id="username"
+        name="username"
+        onChange={(e) => handleCredentialsChange(e, "username")}
+      />
+      <ValidationError errors={errors} errorPath={"username"} />
+      <label htmlFor="password">Password:</label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        onChange={(e) => handleCredentialsChange(e, "password")}
+      />
+      <ValidationError errors={errors} errorPath={"password"} />
+      <label htmlFor="confirm-password">Confirm Password:</label>
+      <input
+        type="password"
+        id="confirm-password"
+        name="confirm-password"
+        onChange={(e) => handleCredentialsChange(e, "confirm-password")}
+      />
+      <ValidationError errors={errors} errorPath={"confirm-password"} />
+      <button disabled={submittedSignUp}>Register</button>
+    </form>
+  );
+}
+
+function ValidationError({ errors, errorPath }) {
+  return (
+    errors && (
+      <ul className="validation-errors">
+        {errors[errorPath] !== undefined &&
+          errors[errorPath].map((err, index) => (
+            <li key={errors[errorPath] + index}>{err}</li>
+          ))}
+      </ul>
+    )
+  );
+}
