caa16139373f1f34f4464f5bd011a59e6226a759
Azam Gulov <gulovazam@gmail.com>
2024-01-28 20:39:45 +0500

    Add username

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,7 +4,10 @@ export default function Nav({ user }: { user: { avatar: string } }) {
   const avatarSrc = `../../public/avatars/${user.avatar}.svg`;
   return (
     <nav className="nav">
-      <img src={avatarSrc} alt="user avatar" className="avatar__nav" />
+      <div className="nav__user">
+        <img src={avatarSrc} alt="user avatar" className="avatar__nav" />
+        <p className="nav__username">{user.username}</p>
+      </div>
       <LogOut />
     </nav>
   );
