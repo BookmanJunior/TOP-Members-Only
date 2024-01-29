1f9a3cf9ced90407e95687ae4895dd893f326817
Azam Gulov <gulovazam@gmail.com>
2024-01-29 21:58:17 +0500

    Add messages component
    scroll down to the first message on fetch

--- src/routes/Message-Board.tsx
+++ src/routes/Message-Board.tsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef, createRef } from "react";
 import { useNavigate, useOutletContext } from "react-router-dom";
-import LogOut from "../components/LogOut";
+import Nav from "../components/Nav";
+import "../styles/message-board.css";
 
 export default function MessageBoard() {
   const [messages, setMessages] = useState([]);
@@ -39,23 +40,14 @@ export default function MessageBoard() {
   }, []);
 
   return (
-    <div>
-      <LogOut />
-      <MessageForm setMessages={setMessages} user={user} />
-      {loadingMessages
-        ? "Loading"
-        : messages.length
-        ? messages.map((message) => {
-            return (
-              <Message
-                key={message.date + message._id}
-                message={message}
-                user={user}
-                setMessages={setMessages}
-              />
-            );
-          })
-        : "No Messages"}
+    <div className="message-board">
+      <Messages messages={messages} user={user} setMessages={setMessages} />
+      <footer>
+        <div className="footer__wrapper">
+          <Nav user={user} />
+          <MessageForm setMessages={setMessages} user={user} />
+        </div>
+      </footer>
     </div>
   );
 }
@@ -64,6 +56,8 @@ function MessageForm({ setMessages, user }) {
   const [userMessage, setUserMessage] = useState("");
   const [messageSubmitted, setMessageSubmitted] = useState(false);
   const [errors, setErrors] = useState();
+  const formRef = useRef<HTMLFormElement>(null);
+  const charactersLeft = 150 - userMessage.length;
 
   async function handleMessageSubmit(e) {
     e.preventDefault();
@@ -97,37 +91,84 @@ function MessageForm({ setMessages, user }) {
   }
 
   return (
-    <form
-      onSubmit={handleMessageSubmit}
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "1em",
-      }}
-    >
-      <textarea
-        name=""
-        id=""
-        cols={50}
-        rows={10}
-        placeholder="Your message here..."
-        value={userMessage}
-        onChange={(e) => setUserMessage(e.target.value)}
-      ></textarea>
-      {errors && (
-        <ul className="errors">
-          {errors.map((error) => (
-            <li key={error.path}>{error.msg}</li>
-          ))}
-        </ul>
-      )}
-      <button disabled={messageSubmitted}>Post</button>
-    </form>
+    <>
+      <form
+        className="message-form"
+        onSubmit={handleMessageSubmit}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "1em",
+        }}
+        ref={formRef}
+      >
+        <div className="textarea-grow-wrap">
+          <textarea
+            name="message"
+            rows={1}
+            maxLength={150}
+            placeholder="Your message here..."
+            value={userMessage}
+            onChange={(e) => setUserMessage(e.target.value)}
+            onKeyDown={(e) => {
+              if (!e.shiftKey && e.key === "Enter") {
+                e.preventDefault();
+                formRef.current?.requestSubmit();
+              }
+            }}
+          ></textarea>
+          <span className="textarea__grow" aria-hidden="true">
+            {`${userMessage} " "`}
+          </span>
+        </div>
+        {/* <span className="message__word-count">{charactersLeft}</span> */}
+        {errors && (
+          <ul className="errors">
+            {errors.map((error) => (
+              <li className="error-msg" key={error.path}>
+                {error.msg}
+              </li>
+            ))}
+          </ul>
+        )}
+        <button disabled={messageSubmitted}>Post</button>
+      </form>
+    </>
+  );
+}
+
+function Messages({ messages, user, setMessages }) {
+  useEffect(() => {
+    if (messages.length) {
+      messageEndRef.current?.scrollIntoView();
+    }
+  }, [messages.length]);
+
+  const messageEndRef = useRef();
+  return messages.length ? (
+    <>
+      <div className="messages">
+        {messages.map((message) => {
+          return (
+            <Message
+              key={message.date + message._id}
+              message={message}
+              user={user}
+              setMessages={setMessages}
+            />
+          );
+        })}
+      </div>
+      <div ref={messageEndRef} className="scroll__to__bottom"></div>
+    </>
+  ) : (
+    <span className="no-messages-text">No Messages</span>
   );
 }
 
 function Message({ message, user, setMessages }) {
-  const isUserAdmin = user.admin;
+  const isUserAdmin = user?.admin;
+  const userAvatar = `avatars/${message?.user?.avatar}.svg`;
   const messageCredentials = {
     messageId: message._id,
   };
@@ -166,16 +207,26 @@ function Message({ message, user, setMessages }) {
 
   return (
     <>
-      <div className="user-message">
-        <div>{message.user.username}</div>
-        <div>{message.message}</div>
-        <div>{message.date}</div>
+      <div className="message">
+        <div className="user-info__message">
+          <img
+            src={userAvatar}
+            alt="user avatar"
+            className="avatar avatar__message"
+          />
+          <div className="username__message">{message?.user?.username}</div>
+        </div>
+        <div
+          className={`user__message user__message--${message?.user?.avatar}`}
+        >
+          {message.message}
+        </div>
+        {isUserAdmin && (
+          <form className="message__delete-form" onSubmit={handleMessageDelete}>
+            <button disabled={deletingMessageStatus}>Delete</button>
+          </form>
+        )}
       </div>
-      {isUserAdmin && (
-        <form onSubmit={handleMessageDelete}>
-          <button disabled={deletingMessageStatus}>Delete</button>
-        </form>
-      )}
     </>
   );
 }
