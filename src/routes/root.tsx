e2a3adbd8415e769879ca4c5d0c3747e0cbe0261
Azam Gulov <gulovazam@gmail.com>
2024-02-09 15:47:42 +0500

    Add initial fetch request
    remove user types
    import user types

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,21 +1,41 @@
-import { useState } from "react";
-import { Outlet, useOutletContext } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Outlet, useOutletContext, useNavigate } from "react-router-dom";
+import { UserProps } from "../Types";
 import "../styles/App.css";
 
-type User = {
-  username: string;
-  admin: boolean;
-  avatar: string;
-  id: string;
-};
-
 type ContextType = {
-  user: User;
-  setUser: (user: User) => void;
+  user: UserProps;
+  setUser: (user: UserProps) => void;
 };
 
 export default function Root() {
-  const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState<UserProps | null>(null);
+  const navigate = useNavigate();
+
+  //login user automatically if jwt token exists.
+  useEffect(() => {
+    async function auth() {
+      try {
+        const res = await fetch("http://localhost:3000/auth/automatic-login", {
+          method: "POST",
+          mode: "cors",
+          credentials: "include",
+          headers: { "Content-Type": "application/json" },
+        });
+
+        if (res.ok) {
+          const { user } = await res.json();
+          setUser(user);
+          navigate("/message-board");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    auth();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
