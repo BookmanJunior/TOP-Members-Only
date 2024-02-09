23addf07ff5714087f6f4ae9737aaf6ae8454f38
Azam Gulov <gulovazam@gmail.com>
2024-02-09 15:47:52 +0500

    Remove fetch request

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,39 +1,8 @@
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import LoginForm from "../components/LoginForm";
-import { useEffect } from "react";
-import { RootStates } from "./root";
 import Logo from "../assets/logo-durarara.svg";
 
 export default function Home() {
-  const { user, setUser } = RootStates();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    async function auth() {
-      try {
-        const res = await fetch("http://localhost:3000/auth/automatic-login", {
-          method: "POST",
-          mode: "cors",
-          credentials: "include",
-          headers: { "Content-Type": "application/json" },
-        });
-
-        if (res.ok) {
-          const { user } = await res.json();
-          setUser(user);
-          navigate("/message-board");
-        }
-      } catch (error) {
-        navigate("/");
-      }
-    }
-
-    if (!user) {
-      auth();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [user]);
-
   return (
     <>
       <img src={Logo} alt="dollars logo" className="logo logo-home" />
