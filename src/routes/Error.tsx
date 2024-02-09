d6a0a82e3b69c45506588452e24b56c720bfd309
Azam Gulov <gulovazam@gmail.com>
2024-02-09 15:50:25 +0500

    Add link back to home

--- src/routes/Error.tsx
+++ src/routes/Error.tsx
@@ -1,1 +1,10 @@
-export default function ErrorElement() {}
+import { Link } from "react-router-dom";
+
+export default function ErrorElement() {
+  return (
+    <>
+      <p>Something Went Wrong</p>
+      <Link to={"/"}>Go Back</Link>
+    </>
+  );
+}
