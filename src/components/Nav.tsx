303499b1f71ec1079919a1096dcc6c3800c913c0
Azam Gulov <gulovazam@gmail.com>
2024-02-09 17:44:22 +0500

    Fix avatar not displaying

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,12 +1,13 @@
 import LogOut from "./LogOut";
 import { UserProps } from "../Types";
+import getAvatarUrl from "../avatar-util";
 
 type userProps = {
   user: UserProps;
 };
 
 export default function Nav({ user }: userProps) {
-  const avatarSrc = `../../public/avatars/${user?.avatar}.svg`;
+  const avatarSrc = getAvatarUrl(`${user.avatar}.svg`);
   return (
     <nav className="nav">
       <div className="nav__user">
